85b4181d49b89d17ff78f276b439cbc5f8af96ab
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-14 11:17:17 +0200

    Head component, Image component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
@@ -8,6 +9,12 @@ export default function FirstPost() {
         <title>First Post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Image 
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="Your Name"
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
