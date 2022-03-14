c920d12fcebf9bc710a71e745f19c7086ac06757
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-14 13:29:41 +0200

    Added a Script component for a third-party script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
@@ -9,6 +10,13 @@ export default function FirstPost() {
         <title>First Post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(
+          'script loaded correctly, window.FB has been populated'
+        )}
+      />
       <Image 
         src="/images/profile.jpg"
         height={144}
