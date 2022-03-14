4f43d5c9b2d8f67480852e54998deb1d852162ae
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-14 15:03:23 +0200

    Added CSS modules and global css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,21 +2,15 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() => console.log(
-          'script loaded correctly, window.FB has been populated'
-        )}
-      />
       <Image 
         src="/images/profile.jpg"
         height={144}
@@ -29,6 +23,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
